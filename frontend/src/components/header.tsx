f2b69fee6f9e33a4343775a60829b2a76507325d
Dawsonkw <dawsonkw@gmail.com>
2024-09-09 10:56:10 -0600

    This has been Pivoted. Did it early so I didnt have to think about how much more difficult it would be later. I'm still gonna be building a ecommerce platform, but I'm going to be now just doing electronic style products. Like best buy but not infringing on copyright. This way I can think less about the JSON and having to parse through all the blah blah blah and I can instead focus on building a high quality app. Also I added a popover component file for the header

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -1,16 +1,23 @@
 import logo from "../assets/shamazonLogoEdit2.png";
 import { categories } from "../lib/constants";
 
+// I might redesign this into just an electronics store. That way I can focus on
+
 function Header() {
   return (
     <header className="bg-gradient-to-b from-purple-500 to-purple-800 h-[200px] max-h-[200px] z-0 flex w-screen justify-between">
       <div className="flex items-center">
         <img src={logo} alt="logo" className="object-fit max-h-[200px]" />
-        <div className="text-white/70 text-4xl font-semibold ml-4">
-          <h1>Shamazon</h1>
+        <div>
+          <h1 className="text-white/70 text-4xl font-semibold ml-4">
+            Shamazon
+          </h1>
+          <p className="text-white/70 text-lg ml-4">
+            Electronics for the Masses
+          </p>
         </div>
       </div>
-      <div className="flex mt-[170px] ">
+      <div className="flex mt-[170px] mx-auto">
         <nav className="flex text-white/70 text-lg font-medium">
           <ul className="flex flex-row">
             {categories.map((category) => (
---
+++ frontend/src/components/menuPopover.tsx
@@ -0,0 +1,6 @@
+// Popover component for menu items when you hover the menu items, this will display a popover dropdown that
+function Menupopover() {
+  return <div></div>;
+}
+
+export default Menupopover;
