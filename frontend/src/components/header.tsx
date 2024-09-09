a22d2c2a09d1501dba37277c854c1fb7d19441b7
Dawsonkw <dawsonkw@gmail.com>
2024-09-09 10:35:21 -0600

    started building out header. Added in a JSON file with a bunch of product info.

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -1,9 +1,27 @@
+import logo from "../assets/shamazonLogoEdit2.png";
+import { categories } from "../lib/constants";
+
 function Header() {
   return (
-    <div className="bg-gradient-to-b from-purple-500 to-purple-800 h-[200px] z-0">
-      <h1 className="text-3xl font-bold underline text-white/50">Shamazon</h1>
-      <h2 className="text-2xl font-bold text-white/50">Welcome to Shamazon!</h2>
-    </div>
+    <header className="bg-gradient-to-b from-purple-500 to-purple-800 h-[200px] max-h-[200px] z-0 flex w-screen justify-between">
+      <div className="flex items-center">
+        <img src={logo} alt="logo" className="object-fit max-h-[200px]" />
+        <div className="text-white/70 text-4xl font-semibold ml-4">
+          <h1>Shamazon</h1>
+        </div>
+      </div>
+      <div className="flex mt-[170px] ">
+        <nav className="flex text-white/70 text-lg font-medium">
+          <ul className="flex flex-row">
+            {categories.map((category) => (
+              <li key={category} className="mr-6">
+                <a href="#">{category}</a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+    </header>
   );
 }
 
---
+++ frontend/src/lib/constants.ts
@@ -0,0 +1,11 @@
+export const categories = [
+  "Electronics",
+  "Clothing",
+  "Home Decor",
+  "Sports & Fitness",
+  "Beauty & Personal Care",
+  "Toys & Games",
+  "Books & Stationery",
+  "Kitchen Appliances",
+  "Footwear",
+];
