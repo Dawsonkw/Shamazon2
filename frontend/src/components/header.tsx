6f33f0d4d8a0b533c231f4f85ca17c32ff1a4c4b
Dawsonkw <dawsonkw@gmail.com>
2024-09-09 11:33:28 -0600

    refactored the header and extracted out the nav section to its own component. Added in framer motion with a placeholder color cause I still havent decided on a color scheme. And Framer-motion is slick looking.

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -1,11 +1,9 @@
 import logo from "../assets/shamazonLogoEdit2.png";
-import { categories } from "../lib/constants";
-
-// I might redesign this into just an electronics store. That way I can focus on
+import Nav from "./nav";
 
 function Header() {
   return (
-    <header className="bg-gradient-to-b from-purple-500 to-purple-800 h-[200px] max-h-[200px] z-0 flex w-screen justify-between">
+    <header className="bg-gradient-to-b from-purple-500 to-purple-800 h-[225px] max-h-[225px] z-0 flex w-screen justify-between">
       <div className="flex items-center">
         <img src={logo} alt="logo" className="object-fit max-h-[200px]" />
         <div>
@@ -18,15 +16,7 @@ function Header() {
         </div>
       </div>
       <div className="flex mt-[170px] mx-auto">
-        <nav className="flex text-white/70 text-lg font-medium">
-          <ul className="flex flex-row">
-            {categories.map((category) => (
-              <li key={category} className="mr-6">
-                <a href="#">{category}</a>
-              </li>
-            ))}
-          </ul>
-        </nav>
+        <Nav />
       </div>
     </header>
   );
---
+++ frontend/src/components/nav.tsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import { motion } from "framer-motion";
+import { categories } from "../lib/constants";
+
+function Nav() {
+  const [activeCategory, setActiveCategory] = useState(categories[0]);
+
+  return (
+    <nav className="flex text-white/70 text-lg font-medium">
+      <ul className="flex flex-row relative">
+        {categories.map((category) => (
+          <li key={category} className="mr-6 relative">
+            {/* This is not going to be an anchor likely since it's gonna have a popover but I doont want to code up the ppopover right now so for now it is what it is  */}
+            <a
+              href="#"
+              className="block py-2 mt-4"
+              onClick={() => setActiveCategory(category)}
+            >
+              {category}
+            </a>
+
+            {location.pathname === `/${category.toLowerCase()}` && (
+              <motion.div
+                layoutId="underline"
+                className="bg-accent h-1 absolute bottom-1 left-0 right-0"
+              />
+            )}
+
+            {activeCategory === category && (
+              <motion.div
+                layoutId="underline"
+                className="bg-accent h-0.5 absolute bottom-1 left-0 right-0"
+              />
+            )}
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
