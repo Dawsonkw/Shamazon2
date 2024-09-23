e70551199fc28417e17ab447401d70f5695e383e
Dawsonkw <dawsonkw@gmail.com>
2024-09-23 16:18:49 -0600

    did some simple file structure layout stuff for future me, and added in some more pics that I forced an AI to create for me.

--- frontend/src/components/hero.tsx
+++ frontend/src/components/hero.tsx
@@ -6,12 +6,18 @@ import TopPicks from "./topPicks";
 //  A hero component section for the home page, modeling after bestbuy landing page, hero section will contain a Large Image to the left, to the right will be Top Picks, on top of a deal of the day and another info section
 function Hero() {
   return (
-    <div>
-      <SplashLogo />
-      <TopPicks />
-      <DealOfTheDay />
-      <InfoSection />
-    </div>
+    <main>
+      <div className="flex flex-row">
+        <SplashLogo />
+        <div className="flex-col">
+          <TopPicks />
+          <div className="flex-row">
+            <DealOfTheDay />
+            <InfoSection />
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
 
