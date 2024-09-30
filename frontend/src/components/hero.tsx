dc570c49b22e379bd28bddd1ad87cd26d838f653
Dawsonkw <dawsonkw@gmail.com>
2024-09-30 16:06:36 -0600

    added in another splashLogo component to keep things seperated easily, added it to the hero section and styled the layout a bit

--- frontend/src/components/hero.tsx
+++ frontend/src/components/hero.tsx
@@ -1,14 +1,15 @@
 import DealOfTheDay from "./dealOfTheDay";
 import InfoSection from "./infoSection";
 import SplashLogo from "./splashLogo";
+import SplashLogo2 from "./splashLogo2";
 import TopPicks from "./topPicks";
 
 //  A hero component section for the home page, modeling after bestbuy landing page, hero section will contain a Large Image to the left, to the right will be Top Picks, on top of a deal of the day and another info section
 function Hero() {
   return (
-    <main className="mx-auto scrollbar-hide">
+    <main className="mx-auto scrollbar-hide flex justify-center">
       <div className="flex flex-row items-center border border-black my-8 mx-8 h-[900px] ">
-        <SplashLogo />
+        <SplashLogo2 />
         <div className="flex flex-col justify-center">
           <div className=" border border-black max-h-[350px] my-8 mx-auto">
             <TopPicks />
@@ -18,6 +19,7 @@ function Hero() {
             <DealOfTheDay />
           </div>
         </div>
+        <SplashLogo />
       </div>
     </main>
   );
---
+++ frontend/src/components/splashLogo2.tsx
@@ -0,0 +1,15 @@
+import splashLogo2 from "../assets/SortingSamuraiElectronics.jpg";
+
+function SplashLogo2() {
+  return (
+    <>
+      <img
+        src={splashLogo2}
+        alt="Splash Logo"
+        className="object-fit max-h-[800px] h-[800px] w-[500px] border-black my-8 mx-8"
+      />
+    </>
+  );
+}
+
+export default SplashLogo2;
