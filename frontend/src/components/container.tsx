e9b6a054745210a92257685f56f9086c52e30d90
Dawsonkw <dawsonkw@gmail.com>
2024-10-06 17:33:23 -0600

    Im not gonna lie, sometimes git version control and my vscode do somethine weird and it just decides to frag one or two files that I was working on and then I have to redo shit and it makes me angry

--- frontend/src/components/container.tsx
+++ frontend/src/components/container.tsx
@@ -1,10 +1,6 @@
 function Container({ children }: { children: React.ReactNode }) {
   return (
-<<<<<<< HEAD
     <main className="container mx-auto px-4 py-8 border border-black -mt-[50px] z-50 relative bg-gray-200/50 rounded-lg h-[930px]">
-=======
-    <main className="container mx-auto px-4 py-8 border border-black -mt-[50px] z-50 relative bg-gray-200/50 rounded-lg">
->>>>>>> acb70e5614c4d9ad1793bad054e7b9db8f4cefb1
       {children}
     </main>
   );
